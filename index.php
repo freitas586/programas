ed7639ee5c93c93c73e9629a0d47ddebad841ae6
freitas586 <gbfreitas586@gmail.com>
2024-07-03 15:29:56 -0300

    nova configuracao

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Controle de Estoque</title>
+    <link href="./css/bootstrap.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+
+    <div class="container">
+ 
+    </div>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
+</body>
+
+
+</html>
+
